93029449957a4f2c24d6e8618b74788000f8e47c
Justin Bee <jbee.appz@gmail.com>
2018-11-22 22:53:04 -0500

    implemented getters and setters for HiveBuilder class

--- src/main/java/HiveBuilder.java
+++ src/main/java/HiveBuilder.java
@@ -8,6 +8,51 @@ package main.java;
  */
 public class HiveBuilder {
     
+    private String beeType;
+    private int food;
+    
+    /**
+     * default constructor for HiveBuilder class.
+     * sets the default beetype to worker
+     * and the default food to 100
+     */
+    public HiveBuilder() {
+        this.beeType = "worker";
+        this.food = 100;
+    }
+    
+    /**
+     * setBeeType sets the type of bee.
+     * @param bType - String for the beetype
+     */
+    public void setBeeType(String bType) {
+        this.beeType = bType;
+    }
+    
+    /**
+     * getBeeType gets the type of bee.
+     * @return this.beeType - returns the type of bee
+     */
+    public String getBeeType() {
+        return this.beeType;
+    }
+    
+    /**
+     * setFood sets the amount of food for the hive.
+     * @param food - int sets the amount of food for the hive
+     */
+    public void setFood(int food) {
+        this.food = food;
+    }
+    
+    /**
+     * getFood returns the amount of food for the hive.
+     * @return this.food - amount of food for the hive
+     */
+    public int getFood() {
+        return this.food;
+    }
+    
     /**
      * BeeHive builder.
      * takes the parameters input and builds a new hive. 
