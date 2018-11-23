34b118a3761d700216f229e1db2d3a1970da8b33
Justin Bee <jbee.appz@gmail.com>
2018-11-22 21:54:20 -0500

    implemented default constructor of warrior bee

--- src/main/java/warriorBee.java
+++ src/main/java/warriorBee.java
@@ -1,5 +1,24 @@
 package main.java;
 
-public class warriorBee {
+/**
+ * warriorBee class is a decorator class of the bee
+ * super class.
+ * @author jbee2 - Justin Bee
+ * @version 1122/2018
+ *
+ */
+public class warriorBee extends Bee {
+    
+    /**
+     * default constructor for the warrior bee.
+     */
+    public warriorBee() {
+        this.life = 100;
+        this.strength = 5;
+        this.hunger = 50;
+        this.harvest = 1;
+        this.build = 1;
+        this.type = "warrior";
+    }
 
 }
